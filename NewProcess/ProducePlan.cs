13d073a3d175d3cd35343a2a155685256191617a
king <king@hi.com>
2022-04-25 18:42:16 +0800

    修改了派工拆更

--- NewProcess/ProducePlan.cs
+++ NewProcess/ProducePlan.cs
@@ -12,7 +12,7 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
     string activityCode = "领料生产";
     private IEngine Engine;
 
-    public ListViewRequest Request { get;  set; }
+    public ListViewRequest Request { get; set; }
 
     public D001419Szlywopbivyrv1d64301ta5xv4_ListViewController(H3.SmartForm.ListViewRequest request) : base(request)
     {
@@ -64,7 +64,7 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
         foreach (string objectId in objectIds)
         {
             H3.DataModel.BizObject flowObject = Tools.BizOperation.New(this.Engine, ProcessFlow.TableCode);
-            H3.DataModel.BizObject dispatchObj = Tools.BizOperation.New(this.Engine, Dispatchs.TableCode);
+            //H3.DataModel.BizObject dispatchObj = Tools.BizOperation.New(this.Engine, Dispatchs.TableCode);
             H3.DataModel.BizObject processObj = Tools.BizOperation.New(this.Engine, RealTimeDynamicProduction.TableCode);
             H3.DataModel.BizObject bizObject = Tools.BizOperation.Load(this.Engine, this.Request.SchemaCode, objectId);
             H3.DataModel.BizObject acObject = Tools.BizOperation.Load(this.Engine, OrderSpecification.TableCode, bizObject[ABCDProcessPlan.OrderSpecificationTable] + string.Empty);
@@ -95,7 +95,7 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             }
             //订单号
             flowObject[ProcessFlow.OrderNumber] = bizObject[ABCDProcessPlan.OrderNumber] + string.Empty;
-            dispatchObj[Dispatchs.OrderNumber] = bizObject[ABCDProcessPlan.OrderNumber] + string.Empty;
+
             //规格号
             //dispatchObj[Dispatchs.SpecificationNumber] = bizObject[ABCDProcessPlan.SpecificationNumber] + string.Empty;
             //订单批次号
@@ -108,33 +108,14 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             //工件号
             flowObject[ProcessFlow.WorkpieceNumber] = bizObject[ABCDProcessPlan.WorkpieceNumber] + string.Empty;
 
-            dispatchObj[Dispatchs.WorkpieceNumber] = bizObject[ABCDProcessPlan.WorkpieceNumber] + string.Empty;
+
             //ID
             flowObject[ProcessFlow.ID] = bizObject[ABCDProcessPlan.ID] + string.Empty;
-            dispatchObj[Dispatchs.ID] = bizObject[ABCDProcessPlan.ID] + string.Empty;
+
             processObj[RealTimeDynamicProduction.ID] = bizObject[ABCDProcessPlan.ID] + string.Empty;
             //产品名称
             flowObject[ProcessFlow.ProductName] = bizObject[ABCDProcessPlan.ProductName] + string.Empty;
-            //以订单规格号相同为条件，查询产品参数表中的车加工类别
-            string mySql = string.Format("Select ObjectId,{0} From i_{1} Where {2} = '{3}'",
-                ProductParameter.ProductMachiningCategory, ProductParameter.TableCode,
-                ProductParameter.OrderSpecificationNumber, orderSpec);
-            DataTable parameterData = this.Engine.Query.QueryTable(mySql, null);
-            if (parameterData != null && parameterData.Rows != null && parameterData.Rows.Count > 0)
-            {
-                dispatchObj["ParameterForm"] = parameterData.Rows[0]["ObjectId"] + string.Empty;
-                //产品参数表
-                // flowObject["F0000206"] = parameterData.Rows[0]["ObjectId"] + string.Empty;
-                // //车加工类别
-                // flowObject["F0000207"] = parameterData.Rows[0][ProductParameter.ProductMachiningCategory] + string.Empty;
-                // //钻加工类别
-                //  flowObject["F0000208"] = parameterData.Rows[0]["ObjectId"] + string.Empty;
-            }
-            else
-            {
-                response.Message = "请确认产品参数表，是否有本产品数据！";
-                continue;
-            }
+
             if (acObject != null && (acObject[OrderSpecification.ProductCategory] + string.Empty) != string.Empty)
             {
                 //赋值产品种类
@@ -151,26 +132,22 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             flowObject[ProcessFlow.SpecificationParameters] = bizObject[ABCDProcessPlan.SpecificationParameters] + string.Empty;
             //数据代码
             flowObject[ProcessFlow.DataCode] = bizObject[ABCDProcessPlan.DataCode] + string.Empty;
-            //派工表-数据代码
-            dispatchObj["F0000070"] = bizObject[ABCDProcessPlan.DataCode] + string.Empty;
+
             //进度管理-数据代码
             processObj["F0000014"] = bizObject[ABCDProcessPlan.DataCode] + string.Empty;
             //部门代码
             flowObject["F0000154"] = bizObject["F0000236"] + string.Empty;
-            //派工表-部门代码
-            dispatchObj["F0000071"] = bizObject["F0000236"] + string.Empty;
+
             //进度管理-部门代码
             processObj["F0000076"] = bizObject["F0000236"] + string.Empty;
             //版本号
             flowObject["F0000153"] = bizObject["F0000235"] + string.Empty;
-            //派工表-版本号
-            dispatchObj["F0000091"] = bizObject["F0000235"] + string.Empty;
+
             //进度管理-版本号
             processObj["F0000077"] = bizObject["F0000235"] + string.Empty;
             //部门多选
             flowObject["F0000152"] = bizObject["F0000234"];
-            //派工表-部门多选
-            dispatchObj["F0000072"] = bizObject["F0000234"];
+
             //进度管理-部门多选
             processObj["F0000078"] = bizObject["F0000234"] + string.Empty;
             //dispatchObj[Dispatchs.DataCode] = bizObject[ABCDProcessPlan.DataCode] + string.Empty;
@@ -199,8 +176,7 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             //计划热处理炉号
             flowObject[ProcessFlow.PlannedHeatTreatmentHeatNumber] = bizObject[ABCDProcessPlan.HeatTreatmentFurnaceNumber] + string.Empty;
 
-            //取样计划完成日期
-            dispatchObj[Dispatchs.SamplingPlanCompletionTime] = bizObject[ABCDProcessPlan.CompletionTimeOfSamplingPlan] + string.Empty;
+
             //热加工计划期
             processObj[RealTimeDynamicProduction.HotProcessingPlanPeriod] = bizObject[ABCDProcessPlan.HotProcessingPlan] + string.Empty;
             //冷加工计划期
@@ -211,8 +187,7 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             processObj[RealTimeDynamicProduction.OrderBatchTable] = bizObject[ABCDProcessPlan.OrderBatchTable] + string.Empty;
             //订单规格表
             processObj[RealTimeDynamicProduction.OrderSpecificationTable] = bizObject[ABCDProcessPlan.OrderSpecificationTable] + string.Empty;
-            //派工-订单规格表
-            dispatchObj["F0000055"] = bizObject[ABCDProcessPlan.OrderSpecificationTable] + string.Empty;
+
             //订单批次规格表
             processObj[RealTimeDynamicProduction.OrderBatchSpecificationTable] = bizObject[ABCDProcessPlan.OrderBatchSpecificationTable] + string.Empty;
             //质量状态
@@ -246,20 +221,20 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             flowObject[ProcessFlow.RoughTurningUnit] = bizObject[ABCDProcessPlan.ProcessingUnitRoughTurning] + string.Empty;
             //本工序需求期-粗车
             flowObject[ProcessFlow.DemandPeriodOfThisOperationRoughTurning] = bizObject[ABCDProcessPlan.RoughTurningPlanCompletionTime] + string.Empty;
-            dispatchObj[Dispatchs.RoughTurningPlanCompletionTime] = bizObject[ABCDProcessPlan.RoughTurningPlanCompletionTime] + string.Empty;
+
 
             //精车加工单位
             flowObject[ProcessFlow.FinishTurningUnit] = bizObject[ABCDProcessPlan.ProcessingUnitFinishTurning] + string.Empty;
 
             //本工序需求期-精车
             flowObject[ProcessFlow.DemandPeriodOfThisProcessFinishTurning] = bizObject[ABCDProcessPlan.FinishTurningPlannedCompletionTime] + string.Empty;
-            dispatchObj[Dispatchs.FinishTurningPlanCompletionTime] = bizObject[ABCDProcessPlan.FinishTurningPlannedCompletionTime] + string.Empty;
+
 
             //钻孔加工单位
             flowObject[ProcessFlow.DrillingUnit] = bizObject[ABCDProcessPlan.ProcessingUnitDrilling] + string.Empty;
             //本工序需求期-钻孔
             flowObject[ProcessFlow.DemandPeriodOfThisProcessDrilling] = bizObject[ABCDProcessPlan.DrillingPlannedCompletionTime] + string.Empty;
-            dispatchObj[Dispatchs.DrillingPlanCompletionTime] = bizObject[ABCDProcessPlan.DrillingPlannedCompletionTime] + string.Empty;
+
 
             //工序计划表
             flowObject[ProcessFlow.OperationSchedule] = bizObject[ABCDProcessPlan.ObjectId] + string.Empty;
@@ -293,15 +268,8 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
 
             //完成总量
             flowObject[ProcessFlow.ToCompleteTheTotalAmount] = 0;
-            //"派工表"赋值工艺流程数据Id
-            dispatchObj["FlowInfo"] = flowObject.ObjectId;
-            //派工表赋值无派工加工审批权限人
-            dispatchObj["F0000085"] = bizObject["F0000252"]; //总计划员
-            dispatchObj["F0000086"] = bizObject["F0000251"]; //冷加工科长
-            dispatchObj["F0000087"] = bizObject["F0000247"]; //取样班组长
-            dispatchObj["F0000088"] = bizObject["F0000248"]; //粗车班组长
-            dispatchObj["F0000089"] = bizObject["F0000249"]; //精车班组长
-            dispatchObj["F0000090"] = bizObject["F0000250"]; //钻孔班组长
+
+
 
             //工艺流程表赋值无派工加工审批权限人
             flowObject["F0000194"] = bizObject["F0000252"]; //总计划员
@@ -312,17 +280,15 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
             flowObject["F0000198"] = bizObject["F0000250"]; //钻孔班组长
 
             processObj.Status = H3.DataModel.BizObjectStatus.Effective;
-            dispatchObj.Status = H3.DataModel.BizObjectStatus.Effective;
+
             //工艺流程表数据
             flowObject.Create();
-            //派工数据
-            dispatchObj.Create();
+
             //"进度管理"赋值工艺流程数据Id
             processObj[RealTimeDynamicProduction.ProcessFlowTable] = flowObject.ObjectId;
             //进度管理数据
             processObj.Create();
-            //"工艺流程"赋值派工信息Id
-            flowObject[ProcessFlow.DispatchingInformation] = dispatchObj.ObjectId;
+
             //"工艺流程"赋值进度管理信息
             flowObject["Progress"] = processObj.ObjectId;
             //工艺流程数据
@@ -346,8 +312,70 @@ public class D001419Szlywopbivyrv1d64301ta5xv4_ListViewController : H3.SmartForm
         {
             return;
         }
+
         foreach (string objectId in objectIds)
         {
+            H3.DataModel.BizObject flowObject = Tools.BizOperation.New(this.Engine, ProcessFlow.TableCode);
+            H3.DataModel.BizObject dispatchObj = Tools.BizOperation.New(this.Engine, Dispatchs.TableCode);
+            //H3.DataModel.BizObject processObj = Tools.BizOperation.New(this.Engine, RealTimeDynamicProduction.TableCode);
+            H3.DataModel.BizObject bizObject = Tools.BizOperation.Load(this.Engine, this.Request.SchemaCode, objectId);
+            //H3.DataModel.BizObject acObject = Tools.BizOperation.Load(this.Engine, OrderSpecification.TableCode, bizObject[ABCDProcessPlan.OrderSpecificationTable] + string.Empty);
+
+
+            dispatchObj[Dispatchs.OrderNumber] = bizObject[ABCDProcessPlan.OrderNumber] + string.Empty;
+            dispatchObj[Dispatchs.WorkpieceNumber] = bizObject[ABCDProcessPlan.WorkpieceNumber] + string.Empty;
+            dispatchObj[Dispatchs.ID] = bizObject[ABCDProcessPlan.ID] + string.Empty;
+
+            //派工表-数据代码
+            dispatchObj["F0000070"] = bizObject[ABCDProcessPlan.DataCode] + string.Empty;
+            //派工表-部门代码
+            dispatchObj["F0000071"] = bizObject["F0000236"] + string.Empty;
+            //派工表-版本号
+            dispatchObj["F0000091"] = bizObject["F0000235"] + string.Empty;
+            //派工表-部门多选
+            dispatchObj["F0000072"] = bizObject["F0000234"];
+
+            //以订单规格号相同为条件，查询产品参数表中的车加工类别
+            string orderSpec = bizObject[ABCDProcessPlan.OrderSpecificationNumber] + string.Empty;
+            string mySql = string.Format("Select ObjectId,{0} From i_{1} Where {2} = '{3}'",
+                ProductParameter.ProductMachiningCategory, ProductParameter.TableCode,
+                ProductParameter.OrderSpecificationNumber, orderSpec);
+            DataTable parameterData = this.Engine.Query.QueryTable(mySql, null);
+            if (parameterData != null && parameterData.Rows != null && parameterData.Rows.Count > 0)
+            {
+                dispatchObj["ParameterForm"] = parameterData.Rows[0]["ObjectId"] + string.Empty;
+            }
+            else
+            {
+                response.Message = "请确认产品参数表，是否有本产品数据！";
+                continue;
+            }
+
+            //取样计划完成日期
+            dispatchObj[Dispatchs.SamplingPlanCompletionTime] = bizObject[ABCDProcessPlan.CompletionTimeOfSamplingPlan] + string.Empty;
+            //派工-订单规格表
+            dispatchObj["F0000055"] = bizObject[ABCDProcessPlan.OrderSpecificationTable] + string.Empty;
+
+            dispatchObj[Dispatchs.RoughTurningPlanCompletionTime] = bizObject[ABCDProcessPlan.RoughTurningPlanCompletionTime] + string.Empty;
+            dispatchObj[Dispatchs.FinishTurningPlanCompletionTime] = bizObject[ABCDProcessPlan.FinishTurningPlannedCompletionTime] + string.Empty;
+            dispatchObj[Dispatchs.DrillingPlanCompletionTime] = bizObject[ABCDProcessPlan.DrillingPlannedCompletionTime] + string.Empty;
+            //"派工表"赋值工艺流程数据Id
+            dispatchObj["FlowInfo"] = flowObject.ObjectId;
+            //派工表赋值无派工加工审批权限人
+            dispatchObj["F0000085"] = bizObject["F0000252"]; //总计划员
+            dispatchObj["F0000086"] = bizObject["F0000251"]; //冷加工科长
+            dispatchObj["F0000087"] = bizObject["F0000247"]; //取样班组长
+            dispatchObj["F0000088"] = bizObject["F0000248"]; //粗车班组长
+            dispatchObj["F0000089"] = bizObject["F0000249"]; //精车班组长
+            dispatchObj["F0000090"] = bizObject["F0000250"]; //钻孔班组长
+
+
+            dispatchObj.Status = H3.DataModel.BizObjectStatus.Effective;
+            //派工数据
+            dispatchObj.Create();
+
+            //"工艺流程"赋值派工信息Id
+            flowObject[ProcessFlow.DispatchingInformation] = dispatchObj.ObjectId;
 
             return;
         }
